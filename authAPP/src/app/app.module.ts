1bef1a2ba8559803ddc61b92c052ffadd542f8f3
kuriapaul <pauwains@gmail.com>
2019-01-20 21:17:34 +0300

    added register ui

--- authAPP/src/app/app.module.ts
+++ authAPP/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { SpecialEventsComponent } from './special-events/special-events.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
