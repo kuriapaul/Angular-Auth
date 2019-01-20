d9806fa316380935621e693f42cf7ed811ba48ac
kuriapaul <pauwains@gmail.com>
2019-01-20 22:46:14 +0300

    added login service

--- authAPP/src/app/auth.service.ts
+++ authAPP/src/app/auth.service.ts
@@ -5,10 +5,15 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class AuthService {
-private _registerUrl = 'http://localhost:3000/api/register'
+private _registerUrl = 'http://localhost:3000/api/register';
+private _loginUrl = 'http://localhost:3000/api/login';
+
   constructor(private http: HttpClient) { }
 
   registerUser(user) {
     return this.http.post<any>(this._registerUrl, user)
+}
+loginUser(user) {
+  return this.http.post<any>(this._loginUrl, user)
 }
 }
