e081759e8f65d0da7ff348500132b18749a7b5f7
kuriapaul <pauwains@gmail.com>
2019-01-24 10:10:18 +0300

    added middleware to verify token

--- server/routes/api.js
+++ server/routes/api.js
@@ -13,6 +13,23 @@ mongoose.connect(db, err => {
     }
 })
 
+function verifyToken(req, res, next) {
+  if(!req.headers.authorization) {
+    return res.status(401).send('Unauthorized request')
+  }
+  let token = req.headers.authorization.split(' ')[1]
+  if(token === 'null') {
+    return res.status(401).send('Unauthorized request')    
+  }
+  let payload = jwt.verify(token, 'secretKey')
+  if(!payload) {
+    return res.status(401).send('Unauthorized request')    
+  }
+  req.userId = payload.subject
+  next()
+}
+
+
 router.get('/', (req, res) => {
     res.send('From API routes')
 })
@@ -94,7 +111,7 @@ router.post('/register', (req, res) => {
     res.json(events)
   })
 
-  router.get('/special', (req, res) => {
+  router.get('/special', verifyToken, (req, res) => {
     let specialEvents = [
       {
         "_id": "1",
